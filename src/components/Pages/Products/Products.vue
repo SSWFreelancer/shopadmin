89c0aa56f30bfc4a6c0b0d4d21a3f32d86b633f6
SSWFreelancer <sswfreelance@mail.ru>
2024-08-26 17:31:43 +0500

    Added Filter & Sort

--- src/components/Pages/Products/Products.vue
+++ src/components/Pages/Products/Products.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="products">
     <Title title="Товары" />
+    <div class="products__filter">
+      <div class="products__column">
+        <span> Сортировать по: </span>
+        <input
+          type="text"
+          v-model="searchQuery"
+          placeholder="Название товара"
+        />
+      </div>
+      <div class="products__column">
+        <span> Цена: </span>
+        <div class="products__rangewrapper">
+          <span> ${{ this.priceRange[0] }} </span>
+          <div class="products__range" ref="rangeSlider"></div>
+          <span> ${{ this.priceRange[1] }} </span>
+        </div>
+      </div>
+      <div class="products__column">
+        <span> Статус: </span>
+        <select v-model="statusFilter">
+          <option value="Любой">Любой</option>
+          <option value="Активный">Активный</option>
+          <option value="Неактивный">Неактивный</option>
+        </select>
+      </div>
+    </div>
     <div class="table">
       <table>
         <thead>
@@ -38,8 +64,15 @@
           </tr>
         </thead>
         <tbody>
+          <td
+            class="table__notfound"
+            v-if="filteredProducts.length === 0"
+            colspan="6"
+          >
+            Нет данных
+          </td>
           <ProductCard
-            v-for="product in products"
+            v-for="product in filteredProducts"
             :key="product.id"
             :imgsrc="product.images[0]?.url"
             :title="product.title"
@@ -57,6 +90,8 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import axios from "axios";
+import noUiSlider from "nouislider";
+import "nouislider/dist/nouislider.css";
 import ProductCard from "../Products/ProductCard.vue";
 import Title from "@/components/ui/Title.vue";
 interface ProductImage {
@@ -86,8 +121,28 @@ export default class Products extends Vue {
   products: Product[] = [];
   loading: boolean = true;
 
+  minPrice: number = 0;
+  maxPrice: number = 250;
+  priceRange: [number, number] = [this.minPrice, this.maxPrice];
   async mounted() {
     await this.fetchProducts();
+    const slider = this.$refs.rangeSlider as HTMLElement & { noUiSlider?: any };
+    noUiSlider.create(slider, {
+      start: [this.minPrice, this.maxPrice],
+      connect: true,
+      step: 1,
+      range: {
+        min: this.minPrice,
+        max: this.maxPrice,
+      },
+    });
+
+    slider.noUiSlider?.on("update", (values: string[]) => {
+      this.priceRange = values.map((value) => parseFloat(value)) as [
+        number,
+        number
+      ];
+    });
   }
 
   async fetchProducts() {
@@ -96,6 +151,19 @@ export default class Products extends Vue {
         "https://course-js.javascript.ru/api/rest/products?_embed=subcategory.category&_sort=title&_order=asc&_start=0&_end=30"
       );
       this.products = response.data;
+      this.minPrice = Math.min(...this.products.map((p) => p.price));
+      this.maxPrice = Math.max(...this.products.map((p) => p.price));
+      this.priceRange = [this.minPrice, this.maxPrice];
+      const slider = this.$refs.rangeSlider as HTMLElement & {
+        noUiSlider?: any;
+      };
+      slider.noUiSlider?.updateOptions({
+        range: {
+          min: this.minPrice,
+          max: this.maxPrice,
+        },
+        start: [this.minPrice, this.maxPrice],
+      });
     } catch (error) {
       console.error("Error fetching products:", error);
     } finally {
@@ -134,5 +202,28 @@ export default class Products extends Vue {
       down: this.sortKey === key && this.sortOrder === -1,
     };
   }
+
+  searchQuery: string = "";
+  statusFilter: string = "Любой";
+
+  get filteredProducts() {
+    return this.products.filter((product) => {
+      const matchesName = product.title
+        .toLowerCase()
+        .includes(this.searchQuery.toLowerCase());
+      const matchesPrice =
+        product.price >= this.priceRange[0] &&
+        product.price <= this.priceRange[1];
+      const matchesStatus =
+        this.statusFilter === "Любой" ||
+        (this.statusFilter === "Активный" && product.status === 1) ||
+        (this.statusFilter === "Неактивный" && product.status === 0);
+
+      return matchesName && matchesPrice && matchesStatus;
+    });
+  }
 }
 </script>
+<style lang="sass">
+@import "@/assets/sass/pages/Products/product.sass"
+</style>
